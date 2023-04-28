a1b844fb4c7994193deb1f3b645c167d2fc44339
AValentinoGPereda <angel_garcia3@usmp.pe>
2023-04-27 22:52:15 -0500

    catalog

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -10,6 +10,7 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
-   public DbSet<Contacto> DataContactos { get; set; }
+   public DbSet<Contacto> DataContactos { get; set; }    
+   public DbSet<Producto> DataProductos { get; set; }
 
 }
