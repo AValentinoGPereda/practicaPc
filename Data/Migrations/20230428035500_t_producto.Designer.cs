e395cd33a8a0d35e163ed762c2e98a487ce520b8
AValentinoGPereda <angel_garcia3@usmp.pe>
2023-04-28 11:33:13 -0500

    intento img

--- Data/Migrations/20230428035500_t_producto.Designer.cs
+++ Data/Migrations/20230428035500_t_producto.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using RepasoPC.Data;
@@ -11,9 +12,11 @@ using RepasoPC.Data;
 namespace RepasoPC.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230428035500_t_producto")]
+    partial class t_producto
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
